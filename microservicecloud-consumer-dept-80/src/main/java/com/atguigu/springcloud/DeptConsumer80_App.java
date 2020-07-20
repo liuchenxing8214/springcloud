d9ac08e6d8c2bb2472fc8f326bc4adc800ae0b18
liuchen <1791354972@qq.com>
2020-07-20 17:18:38 +0800

    spingcloud基础第一次提交

---
+++ microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/DeptConsumer80_App.java
@@ -0,0 +1,17 @@
+package com.atguigu.springcloud;
+
+import com.atguigu.springcloud.myrule.MySelfRule;
+import com.atguigu.springcloud.myrule.RandomRule_ZY;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RibbonClient(name="MICROSERVICECLOUD-DEPT",configuration = RandomRule_ZY.class)
+public class DeptConsumer80_App {
+    public static void main(String[] args) {
+        SpringApplication.run(DeptConsumer80_App.class,args);
+    }
+}
